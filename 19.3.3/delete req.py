5688f4582059c8da7fe215fca8620fdb6749c880
Andrey <127407871+ADSLkoo@users.noreply.github.com>
2023-07-30 22:01:20 +0300

    Delete 19.3.3 directory

--- 19.3.3/delete req.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-res = requests.delete(f"https://petstore.swagger.io/v2/pet/9223372036854265719", headers={'accept': 'application/json'})
-
-response_data = res.json() if 'application/json' in res.headers['Content-Type'] else res.text
-
-print(res.status_code)
-print(response_data)
